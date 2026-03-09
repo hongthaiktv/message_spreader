2678b78d3bb7d8f6a2c1452bf289b4e473616fe5
Thai Nguyen <hongthaiktv@gmail.com>
2026-03-10 02:09:02 +0700

    add puppet random time request

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 const port = 3000;
 const rootDir = path.join(__dirname, "public");
 const websites = require('./websites.json');
-let puppet;
+let puppet = {
+	started: true,
+	total: 0,
+	success: 0,
+	failed: 0,
+	urlFailed: []
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,48 +27,65 @@ app.post("/post", (req, res) => {
 app.post("/puppet", (req, res) => {
 	let reqAct = req.body.action;
 	let msg = "";
-	let counter = 0, errCounter = 0;
 	switch (reqAct) {
 		case "start":
 			msg = 'Puppet starting...';
 			console.log(msg);
-			puppet = setInterval(() => {
-				let rand = Math.floor(Math.random() * 4970);
-				const url = `https://${websites[rand]}`;
-				console.log(url);
-				const options = {
-					url: url,
-					strictSSL: false,
-					headers: {
-						'User-Agent': 'Mozilla/5.0 (Android 15; Mobile; rv:79.0) Gecko/79.0 Firefox/79.0'
-					}
-				};
+			puppet.started = true;
+			async function randRequest(time) {
+				function wait() {
+					return new Promise((resolve) => {
+						let randTime = Math.floor(Math.random() * time + 1);
+						setTimeout(() => {
+							let rand;
+							if (puppet.total <= 100) rand = Math.floor(Math.random() * 100 + 1);
+							else if (puppet.total <= 1000) rand = Math.floor(Math.random() * 1000 + 1);
+							else if (puppet.total <= 10000) rand = Math.floor(Math.random() * 10000 + 1);
+							else rand = Math.floor(Math.random() * 100000 + 1);
+							const url = `https://${websites[rand]}`;
+							console.log(`Rank ${rand}: ${url}`);
+							const options = {
+								url: url,
+								strictSSL: false,
+								headers: {
+									'User-Agent': 'Mozilla/5.0 (Android 15; Mobile; rv:79.0) Gecko/79.0 Firefox/79.0'
+								}
+							};
 
-				request(options, function (error, response, body) {
-					if (error) {
-						console.log(`Failed: ${++errCounter}`);
-						console.error(error);
-// 						res.json({result: error});
-						return;
-					}
-					if (response.statusCode !== 200) {
-						console.log(`Failed: ${++errCounter}`);
-						let err = `Error ${response.statusCode}: ${url}`;
-						console.error(err);
-// 						res.json({result: err});
-					} else {
-						console.log(`Success: ${++counter}`);
-// 						console.log('body:', body.substr(0, 20));
-					}
-				});
-			}, 5000);
+							request(options, function (error, response, body) {
+								++puppet.total;
+								if (error) {
+									puppet.urlFailed.push(url);
+									console.log(`Failed: ${++puppet.failed}`);
+									console.error(error);
+									return;
+								}
+								if (response.statusCode !== 200) {
+									puppet.urlFailed.push(url);
+									console.log(`Failed: ${++puppet.failed}`);
+									let err = `Error ${response.statusCode}: ${url}`;
+									console.error(err);
+								} else {
+									console.log(`Success: ${++puppet.success}`);
+								}
+							});
+							resolve(puppet);
+						}, randTime);
+					});
+				}
+				while (puppet.started) {
+					await wait();
+				}
+			}
+			randRequest(10000);
 			res.json({result: msg});
 			break;
 
 		case "stop":
-			msg = 'Puppet stopped.';
-			clearInterval(puppet);
+			msg = `Puppet stopped with ${puppet.total} request and ${puppet.urlFailed.length} failed.`;
+			puppet.started = false;
 			console.log(msg);
+			console.log('URL failed:', puppet.urlFailed);
 			res.json({result: msg});
 			break;
 
