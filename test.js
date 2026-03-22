2948278e9f53d94cad6faa9140dca2d0f644c81b
Thai Nguyen <hongthaiktv@gmail.com>
2026-03-22 21:34:05 +0700

    update

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-function test(df=11) {
-	console.log(df);
-}
-test();
+let arr = [
+	{name: "AA"},
+	{name: "BB"}
+];
+let obj = {name1: "AA", name2: "BB"};
+let n = 0, stop = 0;
+let t = setTimeout(function () {
+	console.log("3s");
+}, 3000);
+clearTimeout(t);
