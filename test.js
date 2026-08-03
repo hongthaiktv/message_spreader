0d85a899cb1dae0ab3f311b5e131d7144b079782
Thai Nguyen <hongthaiktv@gmail.com>
2026-08-03 20:42:09 +0700

    remove server.js, test.js and pub_test directory

--- test.js
+++
@@ -1,7 +0,0 @@
-let arr = ["str1", "str2", "str3"]
-const rm = arr.splice(1, 1, "added", "add2")
-console.log(rm, rm.length);
-console.log(arr, arr.length);
-// for (const key in arr) {
-// 	console.log(key);
-// }
